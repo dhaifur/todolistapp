c4af287885a9935f6ee2f84e4bfefbd7ca5c48a3
dhaifur <dhaifurr@gmail.com>
2022-11-09 15:52:46 +0700

    initialized flutter_screenutil package

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,7 +1,6 @@
 import 'package:crud_project/app/app.router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sizer/sizer.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class MyApp extends StatelessWidget {
@@ -10,18 +9,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Sizer(builder: ((context, orientation, deviceType) {
-      return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          textTheme: GoogleFonts.poppinsTextTheme(),
-          primarySwatch: Colors.blue,
-        ),
-        navigatorKey: StackedService.navigatorKey,
-        navigatorObservers: [StackedService.routeObserver],
-        onGenerateRoute: StackedRouter().onGenerateRoute,
-        debugShowCheckedModeBanner: false,
-      );
-    }));
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        primarySwatch: Colors.blue,
+      ),
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [StackedService.routeObserver],
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:crud_project/ui/Customs/setup_snackbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app/app.locator.dart';
 import 'app/my_app.dart';
@@ -8,5 +9,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
   setupSnackBar();
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
